2dd088b6650e63479457d8ddb5be29366b63177d
shaishavgandhi05 <shaishgandhi@gmail.com>
2016-08-16 17:24:01 -0400

    Code rebasing
    * More reusability
    
    Signed-off-by: shaishavgandhi05 <shaishgandhi@gmail.com>

---
+++ library/src/main/java/timeparser/com/androidloginbuttons/BaseButton.java
@@ -0,0 +1,120 @@
+package timeparser.com.androidloginbuttons;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.drawable.GradientDrawable;
+import android.util.AttributeSet;
+import android.widget.Button;
+
+/**
+ * Created by Shaishav on 8/16/2016.
+ */
+public class BaseButton extends Button {
+
+    private Bitmap mIcon;
+    private Paint mPaint;
+    private Rect mSrcRect;
+    private int mIconPadding;
+    private int mIconSize;
+    private boolean mIconCenterAligned;
+    private boolean mRoundedCorner;
+
+    public BaseButton(Context context) {
+        super(context);
+    }
+
+    public BaseButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public BaseButton(Context context, AttributeSet attrs, int defStyleAttr,int color,int logo) {
+        super(context, attrs, defStyleAttr);
+        init(context,attrs,logo);
+        setStyle(color,context);
+    }
+
+    public BaseButton(Context context, AttributeSet attrs, int color, int logo){
+        super(context,attrs);
+        init(context, attrs,logo);
+        setStyle(color,context);
+
+    }
+
+    private void setStyle(int color, Context context){
+        if(mRoundedCorner){
+            setBackgroundResource(R.drawable.round_corner);
+            GradientDrawable drawable = (GradientDrawable)getBackground();
+            drawable.setColor(getResources().getColor(color));
+        }
+        else
+            setBackgroundColor(getResources().getColor(color));
+
+        setPadding((int)Utils.convertDpToPixel(30,context),0,(int)Utils.convertDpToPixel(30,context),0);
+
+        setTextColor(Color.WHITE);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        int shift = (mIconSize + mIconPadding) / 2;
+
+        canvas.save();
+        canvas.translate(shift, 0);
+
+        super.onDraw(canvas);
+
+        if (mIcon != null) {
+            float textWidth = getPaint().measureText((String)getText());
+            int left = (int)((getWidth() / 2f) - (textWidth / 2f) - mIconSize - mIconPadding);
+            int top = getHeight()/2 - mIconSize/2;
+
+            if(!mIconCenterAligned)
+                left = 0;
+
+            Rect destRect = new Rect(left, top, left + mIconSize, top + mIconSize);
+            canvas.drawBitmap(mIcon, mSrcRect, destRect, mPaint);
+        }
+
+        canvas.restore();
+    }
+
+    private void init(Context context, AttributeSet attrs, int logo) {
+        TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.IconButton);
+        mIcon = Utils.drawableToBitmap(getResources().getDrawable(logo));
+        mIconSize = (int)Utils.convertDpToPixel(20,context);
+        mIconCenterAligned = true;
+        mRoundedCorner = false;
+        if(attrs.getAttributeValue("http://schemas.android.com/apk/res/android","text")!=null){
+            mIconPadding = (int)Utils.convertDpToPixel(20,context);
+        }
+
+        for (int i = 0; i < array.getIndexCount(); ++i) {
+            int attr = array.getIndex(i);
+            if(attr == R.styleable.IconButton_iconPadding){
+                mIconPadding = array.getDimensionPixelSize(attr, (int)Utils.convertDpToPixel(20,context));
+            }
+            if(attr == R.styleable.IconButton_iconCenterAligned){
+                mIconCenterAligned = array.getBoolean(attr,true);
+            }
+            if(attr == R.styleable.IconButton_iconSize) {
+                mIconSize = array.getDimensionPixelSize(attr, (int) Utils.convertDpToPixel(20, context));
+            }
+            if(attr == R.styleable.IconButton_roundedCorner){
+                mRoundedCorner = array.getBoolean(attr,false);
+            }
+        }
+
+        array.recycle();
+
+        //If we didn't supply an icon in the XML
+        if(mIcon != null){
+            mPaint = new Paint();
+            mSrcRect = new Rect(0, 0, mIcon.getWidth(), mIcon.getHeight());
+        }
+    }
+}
--- library/src/main/java/timeparser/com/androidloginbuttons/GooglePlusButton.java
+++ library/src/main/java/timeparser/com/androidloginbuttons/GooglePlusButton.java
@@ -14,101 +14,22 @@ import android.widget.Button;
 /**
  * Created by Shaishav on 8/15/2016.
  */
-public class GooglePlusButton extends Button {
+public class GooglePlusButton extends BaseButton {
 
-    private Bitmap mIcon;
-    private Paint mPaint;
-    private Rect mSrcRect;
-    private int mIconPadding;
-    private int mIconSize;
-    private boolean mIconCenterAligned;
-    private boolean mRoundedCorner;
 
     public GooglePlusButton(Context context, AttributeSet attrs) {
-        super(context,attrs);
-        init(context, attrs);
-        int color = getResources().getColor(R.color.google);
-        if(mRoundedCorner){
-            setBackgroundResource(R.drawable.round_corner);
-            GradientDrawable drawable = (GradientDrawable)getBackground();
-            drawable.setColor(color);
-        }
-        else
-            setBackgroundColor(color);
-
-        setPadding((int)Utils.convertDpToPixel(30,context),0,(int)Utils.convertDpToPixel(30,context),0);
-
-        setTextColor(Color.WHITE);
-
+        super(context,attrs,R.color.google,R.drawable.google_logo);
 
     }
 
     public GooglePlusButton(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs,defStyle);
-        init(context, attrs);
+        super(context, attrs,defStyle,R.color.google,R.drawable.google_logo);
+
     }
 
     public GooglePlusButton(Context context) {
         super(context);
     }
 
-    @Override
-    protected void onDraw(Canvas canvas) {
-        int shift = (mIconSize + mIconPadding) / 2;
-
-        canvas.save();
-        canvas.translate(shift, 0);
-
-        super.onDraw(canvas);
-
-        if (mIcon != null) {
-            float textWidth = getPaint().measureText((String)getText());
-            int left = (int)((getWidth() / 2f) - (textWidth / 2f) - mIconSize - mIconPadding);
-            int top = getHeight()/2 - mIconSize/2;
-
-            if(!mIconCenterAligned)
-                left = 0;
-
-            Rect destRect = new Rect(left, top, left + mIconSize, top + mIconSize);
-            canvas.drawBitmap(mIcon, mSrcRect, destRect, mPaint);
-        }
-
-        canvas.restore();
-    }
-
-    private void init(Context context, AttributeSet attrs) {
-        TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.IconButton);
-        mIcon = Utils.drawableToBitmap(getResources().getDrawable(R.drawable.google_logo));
-        mIconSize = (int)Utils.convertDpToPixel(20,context);
-        mIconCenterAligned = true;
-        mRoundedCorner = false;
-        if(attrs.getAttributeValue("http://schemas.android.com/apk/res/android","text")!=null){
-            mIconPadding = (int)Utils.convertDpToPixel(20,context);
-        }
-
-        for (int i = 0; i < array.getIndexCount(); ++i) {
-            int attr = array.getIndex(i);
-            if(attr == R.styleable.IconButton_iconPadding){
-                mIconPadding = array.getDimensionPixelSize(attr, (int)Utils.convertDpToPixel(20,context));
-            }
-            if(attr == R.styleable.IconButton_iconCenterAligned){
-                mIconCenterAligned = array.getBoolean(attr,true);
-            }
-            if(attr == R.styleable.IconButton_iconSize) {
-                mIconSize = array.getDimensionPixelSize(attr, (int) Utils.convertDpToPixel(20, context));
-            }
-            if(attr == R.styleable.IconButton_roundedCorner){
-                mRoundedCorner = array.getBoolean(attr,false);
-            }
-        }
-
-        array.recycle();
-
-        //If we didn't supply an icon in the XML
-        if(mIcon != null){
-            mPaint = new Paint();
-            mSrcRect = new Rect(0, 0, mIcon.getWidth(), mIcon.getHeight());
-        }
-    }
 
 }
